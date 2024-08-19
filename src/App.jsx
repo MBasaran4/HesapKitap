4a8dc07dfbd0b9dcd17198ec3f1956d5df57a90e
MBasaran4 <153395624+MBasaran4@users.noreply.github.com>
2024-08-19 10:00:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import MainPage from './components/MainPage'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <>
+      <Analytics/>
       <MainPage/>
     </>
   )
