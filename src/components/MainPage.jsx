490cc3a8692a064697cb01ab77fec9b29bfe7816
Mücahit <basaranmucahit785@gmail.com>
2024-08-21 14:10:12 +0300

    Navbar Update

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './MainPage.css';
 import Body from './Body';
 import { Analytics } from "@vercel/analytics/react"
